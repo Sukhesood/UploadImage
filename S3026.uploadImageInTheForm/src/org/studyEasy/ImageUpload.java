43eafdc83d0c95c825b3ead508f328e6d033041c
hp <hp@LAPTOP-6USSJ5L9>
2020-10-30 22:52:21 -0400

    Upload image in the image

---
+++ S3026.uploadImageInTheForm/src/org/studyEasy/ImageUpload.java
@@ -0,0 +1,48 @@
+package org.studyEasy;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+/**
+ * Servlet implementation class ImageUpload
+ */
+@WebServlet("/ImageUpload")
+public class ImageUpload extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		ServletFileUpload upload= new ServletFileUpload(new DiskFileItemFactory());
+		try {
+			List<FileItem> images=upload.parseRequest(request);
+			for(FileItem image:images) {
+				String name=image.getName();
+				name=name.substring(name.lastIndexOf("\\")+1)  ;
+				
+				image.write(new File("c:/images/"+name));
+			}
+		} catch (FileUploadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+}
+}
